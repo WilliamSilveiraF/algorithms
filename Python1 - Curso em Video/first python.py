53586aa4c28f98c2b6c2978110cc4723fd2e8040
William Silveira <williamsilveiraf@hotmail.com>
2021-10-27 19:06:04 -0300

    first commit of python

---
+++ Python1 - Curso em Video/first python.py
@@ -0,0 +1,8 @@
+'''
+nome = input('Qual é seu nome? ')
+print('Boas-vindas ' + nome)
+'''
+Dia = input('Dia ')
+Mes = input('Mes ')
+Ano = input('Ano ')
+print('Você nasceu no dia ' + Dia +' de ' + Mes +' de ' + Ano)
