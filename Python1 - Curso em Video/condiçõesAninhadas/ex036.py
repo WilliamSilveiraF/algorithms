7b9d780e3396702bee038023ad6499d71c96586c
William Silveira <williamsilveiraf@hotmail.com>
2021-11-17 09:46:32 -0300

    small fix

--- Python1 - Curso em Video/condiçõesAninhadas/ex036.py
+++ Python1 - Curso em Video/condiçõesAninhadas/ex036.py
@@ -2,7 +2,8 @@ houseValue = float(input('Qual o valor da casa: '))
 buyerIncome = float(input('Qual o salário mensal do comprador: '))
 timeSizeYear = float(input('Tamanho do financiamento em anos:'))
 answer = buyerIncome * 0.30 * 12 * timeSizeYear
+parcela = houseValue / (timeSizeYear * 12)
 if answer > houseValue:
-    print('Parabéns! Empréstimo aprovado!')
+    print('Parabéns! Empréstimo aprovado com parcela fixa no valor de R$ {:.2f} mensais!'.format(parcela))
 else:
     print('Empréstimo negado!')
