5c2cbac14bee7907006a107bc5c1128221617b8e
William Silveira <williamsilveiraf@hotmail.com>
2021-11-20 16:16:02 -0300

    near to the finish

--- Selenium Web Scraping/currency.py
+++ Selenium Web Scraping/currency.py
@@ -11,8 +11,24 @@ driver.implicitly_wait(10)
 elements = driver.find_elements_by_class_name('html-attribute-value')
 
 usdValue = float(elements[4].text)
+todayDate = str(elements[2].text)
+
+import csv
+
+fields = ['Currency Code', 'Rate']
+
+rows = []
 
 for currencyPosition in range(5, len(elements), 2):
     currency = elements[currencyPosition].text
-    value = float(elemen ts[currencyPosition + 1].text)
-    print(f"Currency: {currency}  Value: {value/usdValue}")
+    value = float(elements[currencyPosition + 1].text)
+    rows.append([f"{currency}", f"{(value/usdValue):.3f}"])
+
+filename = "usd_currency_rates_{"+ todayDate +"}.csv"
+
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+
+    csvwriter.writerow(fields)
+
+    csvwriter.writerows(rows)
